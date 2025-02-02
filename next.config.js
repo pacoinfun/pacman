71331ac4b5f1a6cecfb255be999af1c903a29824
rubymaker1 <emrefutures@gmail.com>
2025-02-02 18:20:54 +0200

    added

--- next.config.js
+++ next.config.js
@@ -1,3 +1,15 @@
-module.exports = {
-    trailingSlash: true,
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  trailingSlash: true,
+  async rewrites() {
+    return [
+      {
+        source: '/:path*',
+        destination: '/:path*',
+      },
+    ]
+  },
 }
+
+module.exports = nextConfig
