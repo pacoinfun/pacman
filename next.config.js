5787bc4bbde8cad7215c327e6876e8e524f02cd4
rubymaker1 <emrefutures@gmail.com>
2025-02-02 18:24:56 +0200

    added

--- next.config.js
+++ next.config.js
@@ -5,10 +5,30 @@ const nextConfig = {
   async rewrites() {
     return [
       {
+        source: '/',
+        destination: '/index.html',
+      },
+      {
         source: '/:path*',
         destination: '/:path*',
-      },
+      }
     ]
+  },
+  // Statik dosyaları işlemek için
+  webpack: (config, { isServer }) => {
+    // Statik dosya işleme kuralları
+    config.module.rules.push({
+      test: /\.(mp3|wav|png|jpg|gif|svg)$/,
+      use: {
+        loader: 'file-loader',
+        options: {
+          publicPath: '/_next/static/',
+          outputPath: 'static/',
+        },
+      },
+    });
+
+    return config;
   },
 }
 
